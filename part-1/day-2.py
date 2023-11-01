26da97fba6f8fc0a81f45947a3ddb6428390836a
Engineerbabu777 <awaismumtaz0099@gmail.com>
2023-11-01 18:06:37 +0500

    day-2

---
+++ part-1/day-2.py
@@ -0,0 +1,22 @@
+
+
+
+# CHAR TO ASCI!!
+print(ord('a'));
+
+# ASCII TO CHAR!!
+print(chr(97));
+
+# GET USER INPUT!!
+num = int(input('Enter Number: '));
+print(num+10)
+
+
+# SUM OF TWO GIVEN NUMBERS!
+num1 = int(input('Enter Number(1): '));
+num2 = int(input('Enter Number(2): '));
+
+print(num1 + num2);
+
+
+# ABOUT OPERATORS!
